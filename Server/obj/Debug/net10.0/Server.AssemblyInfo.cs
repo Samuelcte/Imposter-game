db11edb57c60a6a48e2b725f3288bef85eb3e83f
Samuel_cte-Laptop <147095372+Samuelcte@users.noreply.github.com>
2026-05-04 07:58:41 +0200

    Commit

--- Server/obj/Debug/net10.0/Server.AssemblyInfo.cs
+++ Server/obj/Debug/net10.0/Server.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4c10fd76282fdf573ab768c0af5624bbed31c4b3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5b6a2cd0f96a47f8bdbb588dbd89da417ace6125")]
 [assembly: System.Reflection.AssemblyProductAttribute("Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
